268b58bf15fb634e0f8c349f38c4245720aa1e99
Scrap <105889363+scrapp08@users.noreply.github.com>
2023-08-12 16:21:46 +0100

    Final Commit
    Final commit before switching to Unity

--- assets/island/scripts/map_display.gd
+++ assets/island/scripts/map_display.gd
@@ -41,14 +41,16 @@ extends Node3D
 		regions = value
 		display_noise_map()
 @export_category("Mesh")
-@export_range(0.0, 1.0, 0.01) var height_scale : float :
+@export_range(0.01, 1.0, 0.01) var height_scale : float :
 	set(value):
 		height_scale = value
 		display_noise_map()
 
 @onready var mesh_instance := $MeshInstance3D
 @onready var mesh_shader = preload("res://assets/island/shaders/island_mesh.gdshader")
-
+@onready var texture_material := StandardMaterial3D.new()
+@onready var mesh_material := ShaderMaterial.new()
+@onready var mesh_noise := NoiseTexture2D.new()
 
 func _ready() -> void:
 	display_noise_map()
@@ -60,19 +62,14 @@ func display_noise_map() -> void:
 	var noise_map := NoiseGenerator.generate_noise_map(noise_scale, octaves, persistance, lacunarity, map_seed, map_offset)
 # Convert noise to texture & apply colour
 	var noise_texture := TextureGenerator.generate_noise_texture(noise_map, map_size.x, map_size.y, map_offset.x, map_offset.y, colour, regions)
-	var texture_material := StandardMaterial3D.new()
 	texture_material.texture_filter = BaseMaterial3D.TEXTURE_FILTER_NEAREST_WITH_MIPMAPS
 	texture_material.albedo_texture = noise_texture
 	
 	mesh_instance.set_surface_override_material(0, texture_material)
 	mesh_instance.mesh.size = map_size
 # Mesh
-	var mesh_material := ShaderMaterial.new()
-	var mesh_noise := NoiseTexture2D.new()
 	mesh_noise.set_noise(noise_map)
 	mesh_instance.mesh.set_material(mesh_material)
 	mesh_material.set_shader(mesh_shader)
 	mesh_material.set_shader_parameter("noise", mesh_noise)
-	
-	
-# TODO: DON'T GENERATE VAR'S INSIDE display_noise_map() FOR PERFORMANCE, SET HEIGHT_SCALE FROM INSPECTOR
+	mesh_material.set_shader_parameter("height_scale", height_scale)
