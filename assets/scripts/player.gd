e985ce06e219b68745823a24a561500559ea8acd
scrapp08 <105889363+scrapp08@users.noreply.github.com>
2023-09-23 11:04:29 +0100

    Adding in multiplayer cause can't do waves in lessons.

--- assets/scripts/player.gd
+++ assets/scripts/player.gd
@@ -1,27 +1,45 @@
 extends CharacterBody3D
 
-const SPEED := 10.0
-const JUMP_VELOCITY := 10.0
+signal health_changed(health_value)
 
-@onready var camera_3d := $Camera3D
+@onready var camera_3d: Camera3D = $Camera3D
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
+@onready var muzzle_flash: GPUParticles3D = $Camera3D/Pistol/MuzzleFlash
+@onready var ray_cast_3d: RayCast3D = $Camera3D/RayCast3D
+
+const SPEED = 10.0
+const JUMP_VELOCITY = 10.0
 
 var gravity: float = 20.0
-var health := 3
+var health = 3
+
+
+func _enter_tree() -> void:
+	set_multiplayer_authority(str(name).to_int())
 
 
 func _ready() -> void:
+	if not is_multiplayer_authority(): return
 	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
 	camera_3d.current = true
 
 
 func _unhandled_input(event: InputEvent) -> void:
+	if not is_multiplayer_authority(): return
 	if event is InputEventMouseMotion:
 		rotate_y(-event.relative.x * .0025)
 		camera_3d.rotate_x(-event.relative.y * .0025)
 		camera_3d.rotation.x = clamp(camera_3d.rotation.x, -PI/2, PI/2)
+	if Input.is_action_just_pressed("shoot") \
+		and animation_player.current_animation != "shoot":
+		play_shoot_effects.rpc()
+		if ray_cast_3d.is_colliding():
+			var hit_player = ray_cast_3d.get_collider()
+			hit_player.recieve_damage.rpc_id(hit_player.get_multiplayer_authority())
 
 
 func _physics_process(delta: float) -> void:
+	if not is_multiplayer_authority(): return
 	# Add the gravity.
 	if not is_on_floor():
 		velocity.y -= gravity * delta
@@ -37,4 +55,32 @@ func _physics_process(delta: float) -> void:
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 		velocity.z = move_toward(velocity.z, 0, SPEED)
+	if animation_player.current_animation == "shoot":
+		pass
+	elif input_dir != Vector2.ZERO and is_on_floor():
+		animation_player.play("move")
+	else:
+		animation_player.play("idle")
 	move_and_slide()
+
+
+@rpc("call_local")
+func play_shoot_effects():
+	animation_player.stop()
+	animation_player.play("shoot")
+	muzzle_flash.restart()
+	muzzle_flash.emitting = true
+
+
+@rpc("any_peer")
+func recieve_damage():
+	health -= 1
+	if health <= 0:
+		health = 3
+		position = Vector3.ZERO
+	health_changed.emit(health)
+
+
+func _on_animation_player_animation_finished(anim_name: StringName) -> void:
+	if anim_name == "shoot":
+		animation_player.play("idle")
