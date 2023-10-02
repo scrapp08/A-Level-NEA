02f0c1507748d5d6ed02346f808c29d01b0218bb
scrapp08 <105889363+scrapp08@users.noreply.github.com>
2023-10-02 20:53:45 +0100

    Added multiplayer properly

---
+++ assets/scripts/player.gd
@@ -0,0 +1,86 @@
+extends CharacterBody3D
+
+signal health_changed(health_value)
+
+const SPEED = 10.0
+const JUMP_VELOCITY = 10.0
+
+var gravity: float = 20.0
+var health = 3
+
+@onready var camera_3d: Camera3D = $Camera3D
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
+@onready var muzzle_flash: GPUParticles3D = $Camera3D/Pistol/MuzzleFlash
+@onready var ray_cast_3d: RayCast3D = $Camera3D/RayCast3D
+
+
+func _enter_tree() -> void:
+	set_multiplayer_authority(str(name).to_int())
+
+
+func _ready() -> void:
+	if not is_multiplayer_authority(): return
+	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+	camera_3d.current = true
+
+
+func _unhandled_input(event: InputEvent) -> void:
+	if not is_multiplayer_authority(): return
+	if event is InputEventMouseMotion:
+		rotate_y(-event.relative.x * .0025)
+		camera_3d.rotate_x(-event.relative.y * .0025)
+		camera_3d.rotation.x = clamp(camera_3d.rotation.x, -PI/2, PI/2)
+	if Input.is_action_just_pressed("attack_primary") \
+		and animation_player.current_animation != "shoot":
+		play_shoot_effects.rpc()
+		if ray_cast_3d.is_colliding():
+			var hit_player = ray_cast_3d.get_collider()
+			hit_player.recieve_damage.rpc_id(hit_player.get_multiplayer_authority())
+
+
+func _physics_process(delta: float) -> void:
+	if not is_multiplayer_authority(): return
+	# Add the gravity.
+	if not is_on_floor():
+		velocity.y -= gravity * delta
+	# Handle Jump.
+	if Input.is_action_just_pressed("move_up") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+	# Get the input direction and handle the movement/deceleration.
+	var input_dir := Input.get_vector("move_left", "move_right", "move_forward", "move_backward")
+	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	if direction:
+		velocity.x = direction.x * SPEED
+		velocity.z = direction.z * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity.z = move_toward(velocity.z, 0, SPEED)
+	if animation_player.current_animation == "shoot":
+		pass
+	elif input_dir != Vector2.ZERO and is_on_floor():
+		animation_player.play("move")
+	else:
+		animation_player.play("idle")
+	move_and_slide()
+
+
+@rpc("call_local")
+func play_shoot_effects():
+	animation_player.stop()
+	animation_player.play("shoot")
+	muzzle_flash.restart()
+	muzzle_flash.emitting = true
+
+
+@rpc("any_peer")
+func recieve_damage():
+	health -= 1
+	if health <= 0:
+		health = 3
+		position = Vector3.ZERO
+	health_changed.emit(health)
+
+
+func _on_animation_player_animation_finished(anim_name: StringName) -> void:
+	if anim_name == "shoot":
+		animation_player.play("idle")
