ebb3430fd7fab85d64b354c57ac5f965b5fca09f
scrapp08 <105889363+scrapp08@users.noreply.github.com>
2024-01-09 20:37:02 +0000

    makin progress on the collision hell yeah

---
+++ objects/player.gd
@@ -0,0 +1,57 @@
+extends CharacterBody3D
+
+@export_group("Properties")
+@export var movement_speed := 6
+@export var jump_strength := 9
+
+@export_group("Weapons")
+@export var weapons: Array[Weapon] = []
+
+var weapon: Weapon
+var weapon_index := 0
+var container_offset = Vector3(1.2, -1.1, -2.75)
+var tween:Tween
+
+var mouse_sensitivity := 700
+var movement_velocity: Vector3
+var gravity: float = ProjectSettings.get_setting("physics/3d/default_gravity")
+
+@onready var camera := $Head/Camera
+@onready var ray_cast := $Head/Camera/RayCast
+@onready var muzzle = $Head/Camera/SubViewportContainer/SubViewport/CameraItem/Muzzle
+@onready var container = $Head/Camera/SubViewportContainer/SubViewport/CameraItem/Container
+
+@export var crosshair: TextureRect
+
+
+func _ready() -> void:
+	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+
+	#weapon = weapons[weapon_index]
+#w	initiate_change_weapon(weapon_index)
+
+
+func _physics_process(delta: float) -> void:
+	var input := Input.get_vector("move_left", "move_right", "move_forward", "move_backward")
+	movement_velocity = transform.basis * Vector3(input.x, 0, input.y).normalized() * movement_speed
+
+	if Input.is_action_just_pressed("move_up") and is_on_floor():
+		gravity = -jump_strength
+
+	gravity += 20 * delta
+
+	if gravity > 0 and is_on_floor():
+		gravity = 0
+
+	velocity = velocity.lerp(movement_velocity, delta * 10)
+	velocity.y = -gravity
+	move_and_slide()
+
+
+func _unhandled_input(event: InputEvent) -> void:
+	if event is InputEventMouseMotion:
+		rotation.y -= event.relative.x / mouse_sensitivity
+		camera.rotation.x -= event.relative.y / mouse_sensitivity
+		camera.rotation.x = clamp(camera.rotation.x, deg_to_rad(-90), deg_to_rad(90))
+
+
---
+++ scripts/collider.gd
@@ -0,0 +1,100 @@
+@tool
+class_name ColliderGen
+extends StaticBody3D
+
+# Generation data
+var size : int
+var resolution : int
+var amplitude : float
+var noise : NoiseTexture2D
+var gen_falloff : bool
+var falloff_start : float
+var falloff_end : float
+var render_vertices : bool
+
+# Collision
+var height_map := HeightMapShape3D.new()
+var map_data : PackedFloat32Array = []
+
+
+func generate_collision(data) -> void:
+	# Unpack generation data
+	size = data[0]
+	height_map.map_depth = size
+	height_map.map_width = size
+
+	resolution = data[1]
+	amplitude = data[2]
+	noise = data[3]
+	gen_falloff = data[4]
+	falloff_start = data[5]
+	falloff_end = data[6]
+	render_vertices = data[7]
+
+	var increment : float = float(size)/(resolution + 1)
+	var bottom_left := Vector3(size * -0.5, 1, size * -0.5)
+	var xz := Vector3.ZERO
+
+	for x_step in range(resolution + 2):
+		xz.x = bottom_left.x + x_step * increment
+		for z_step in range(resolution + 2):
+			xz.z = bottom_left.z + z_step * increment
+			xz.y = _world_to_texture_value(xz) * amplitude * 2.5
+			if gen_falloff:
+				xz.y *= _falloff(xz)
+				#map_data.append(value * amplitude * _falloff(xz.x, xz.z) * 2.5)
+			if render_vertices:
+				var sphere_mesh : MeshInstance3D = _spawn_sphere(xz)
+				add_child(sphere_mesh)
+
+
+func _world_to_texture_coordinate(coordinate: Vector3) -> Vector2i:
+	var bottom_left := Vector2(size * -0.5, size * -0.5)
+	var top_right := Vector2(size * 0.5, size * 0.5)
+	var noise_width := noise.width
+	var noise_height := noise.height
+	var texture_coordinate : Vector2i
+	texture_coordinate.x = int(remap(coordinate.x, bottom_left.x, top_right.x, 0.0, noise_width))
+	texture_coordinate.y = int(remap(coordinate.z, bottom_left.y, top_right.y, 0.0, noise_height))
+	return texture_coordinate
+
+
+func _world_to_texture_value(coordinate: Vector3) -> float:
+	var bottom_left := Vector2(size * -0.5, size * -0.5)
+	var top_right := Vector2(size * 0.5, size * 0.5)
+	var noise_width := noise.width
+	var noise_height := noise.height
+	var texture_coordinate : Vector2i
+	texture_coordinate.x = int(remap(coordinate.x, bottom_left.x, top_right.x, 0.0, noise_width))
+	texture_coordinate.y = int(remap(coordinate.z, bottom_left.y, top_right.y, 0.0, noise_height))
+	print(noise.noise.get_image(noise_width, noise_height))
+	return noise.noise.get_noise_2dv(texture_coordinate)
+
+
+func _falloff(world_coordinate: Vector3) -> float:
+	var coord_x := remap(world_coordinate.x, float(size) * -0.5, float(size) * 0.5, 1.0, -1.0)
+	var coord_z := remap(world_coordinate.z, float(size) * -0.5, float(size) * 0.5, 1.0, -1.0)
+
+	var t : float = max(abs(coord_x), abs(coord_z))
+	if t <= falloff_start:
+		return 1.0
+	elif t > falloff_end:
+		return 0.0
+	else:
+		return smoothstep(1.0, 0.0, inverse_lerp(falloff_start, falloff_end, t))
+
+
+func _srgb_to_linear(c) -> Vector3:
+	return c * (c * (c * 0.305306011 + 0.682171111) + 0.012522878)
+
+
+func _spawn_sphere(position: Vector3) -> MeshInstance3D:
+	var instance = MeshInstance3D.new()
+	instance.position = position
+	var sphere = SphereMesh.new()
+	sphere.radius = 0.1
+	sphere.height = 0.2
+	instance.mesh = sphere
+	return instance
+
+
