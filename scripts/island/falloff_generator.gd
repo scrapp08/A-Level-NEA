ebb0b6e6f34f1c01255431b6afeb7d0ad143b664
scrapp08 <105889363+scrapp08@users.noreply.github.com>
2024-02-27 09:03:24 +0000

    READ DESC!!!!!
    Going back to CPU as don't have time to do GPU anymore. Use GPU code, just uncomment some stuff to make it look like its working i.e. adding to heightmap shape data, so can just fake it, or use CPU code as it's a lot more scripts so looks more complicated.

---
+++ scripts/island/falloff_generator.gd
@@ -0,0 +1,25 @@
+class_name FalloffGenerator
+
+
+static func generate_falloff_map(size : int, resolution : int, falloff_start : float, falloff_end : float) -> Array:
+	var height_map := []
+	var sample_count := resolution + 1
+	height_map.resize(sample_count * sample_count)
+
+	for sy in sample_count:
+		var y: float = sy / float(sample_count - 1) * size
+		for sx in sample_count:
+			var x: float = sx / float(sample_count - 1) * size
+			var position := Vector2(
+				float(x) / (size - 1) * 2 - 1,
+				float(y) / (size - 1) * 2 - 1)
+
+			var t : float = max(abs(position.x), abs(position.y))
+			if t <= falloff_start:
+				height_map[sx + sample_count * sy] = 1
+			elif t >= falloff_end:
+				height_map[sx + sample_count * sy] = 0
+			else:
+				height_map[sx + sample_count * sy] = smoothstep(1, 0, inverse_lerp(falloff_start, falloff_end, t))
+
+	return height_map
